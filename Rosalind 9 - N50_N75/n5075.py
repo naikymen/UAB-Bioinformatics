74305df32dc553faec7ce1e52e8cd5901c7b5ed2
locomia <mail@falso.com>
2016-11-16 12:41:56 +0100

    wrong

--- Rosalind 9 - N50_N75/n5075.py
+++ Rosalind 9 - N50_N75/n5075.py
@@ -1,8 +1,10 @@
-file = '/home/naikymen/PycharmProjects/UAB/Bioinformatica/Rosalind 9 - N50_N75/input.txt'
+file = '/home/naikymen/PycharmProjects/UAB/Bioinformatica/Rosalind 9 - N50_N75/input3.txt'
+handle = open(file,'r')
+records = handle.readlines()
 
-records = open(file,'r').readlines()
-
-i = []
+lengths = []
+a = []
+b = []
 total = 0
 l = 0
 k = 0
@@ -10,20 +12,31 @@ k = 0
 for record in records:
     length = len(record.rstrip('\n'))
     total += length
-    i.append(length)
+    lengths.append(length)
 
-j = iter(sorted(i))
+sortedLengths = iter(sorted(lengths))
+# https://wiki.python.org/moin/HowTo/Sorting
 
-while l <= total*(50/100)-k:
-    k = next(j)
+while l <= total*(50/100):
+    k = next(sortedLengths)
     l += k
-
+    print(l, k, l / total)
 n50 = k
+print('n50 --',k)
 
-while l <= total*(75/100)-k:
-    k = next(j)
+while l <= total*(75/100):
+    k = next(sortedLengths)
     l += k
-
+    print(l, k, l / total)
 n75 = k
+print('n75 --',k)
 
+while l <= total*(100/100)-k:
+    k = next(sortedLengths)
+    l += k
+    print(l,k,l/total)
+n100 = k
+print('n100 --',k)
+
+print('\n')
 print(n50,n75)
